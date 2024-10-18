8e58996e006fbb6a2aef5b2ff870d367ef8597f4
akashsethia007 <akashsethia007@gmail.com>
2024-10-18 20:14:31 +0530

    Updated the structure of the overall project

--- Code/start_here.py
+++ Code/start_here.py
@@ -1,5 +1,6 @@
 from utils import xirr
 print("Hello world")
+'''
 #phase 1 - > import the list of top 500 companies listed on NSe by marcket cap
 #import the historical data for 1year
 #run ST7(2/3)
@@ -7,3 +8,4 @@ print("Hello world")
 #phase2 - > check for the ST
 #generate GTT
 #phase3 - > Integrate with Kite
+'''
