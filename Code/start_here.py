ae064e7fbac7f9238fa1c94cef712b547131b927
akashsethia007 <akashsethia007@gmail.com>
2024-10-18 23:28:42 +0530

    First Commit

--- Code/start_here.py
+++ Code/start_here.py
@@ -13,11 +13,13 @@ from nsepy.urls import equity_symbol_list_url, index_constituents_url
 from utils import scraper
 from utils import stocks
 import datetime
+
+print("Setting up the dates for this execution")
 date_now = datetime.date.today()
 years_to_add = date_now.year - 1
-
 today_date = date_now.strftime('%Y-%m-%d')
 hist_date = date_now.replace(year=years_to_add).strftime('%Y-%m-%d')
+print(f"Start date set to {today_date} and end date set to {hist_date}")
 
 df = stocks.get_data(stock_symbol="RELIANCE", start_date=hist_date, end_date=today_date)
 print(df.to_string())
