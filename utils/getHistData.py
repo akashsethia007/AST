e79af49d9a1166138f91087b8163325ee26178a7
akashsethia007 <akashsethia007@gmail.com>
2024-10-18 23:51:26 +0530

    First Commit

---
+++ utils/getHistData.py
@@ -0,0 +1,18 @@
+from utils import scraper
+from utils import stocks
+import datetime
+import os
+
+def getHistDatanow(ticker):
+    print("Setting up the dates for this execution")
+    date_now = datetime.date.today()
+    years_to_add = date_now.year - 1
+    today_date = date_now.strftime('%Y-%m-%d')
+    hist_date = date_now.replace(year=years_to_add).strftime('%Y-%m-%d')
+    print(f"Start date set to {today_date} and end date set to {hist_date}")
+    cwd = os.getcwd()
+    new_path = '\\'.join(cwd.split('\\')[:-1]) + '\data\historicalData'
+    df = stocks.get_data(stock_symbol="RELIANCE", start_date=hist_date, end_date=today_date)
+    print("Saving the data to the data location now")
+    df.to_csv(new_path, ticker+".csv")
+    return df
