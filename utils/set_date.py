64deebd484ea2eefd6e38abacfa2adfd05f92e08
akashsethia007 <akashsethia007@gmail.com>
2024-10-21 06:49:04 +0530

    Updated the dates

--- utils/set_date.py
+++ utils/set_date.py
@@ -2,8 +2,5 @@ import datetime
 from datetime import datetime, timedelta
 
 def set_dates(num_of_days):
-    print("Setting up the dates for this execution")
-    today_date = datetime.today().strftime('%Y-%m-%d')
-    hist_date = (datetime.today() - timedelta(days=num_of_days)).strftime('%Y-%m-%d')
-    print(f"Start date set to {today_date} and end date set to {hist_date}")
-    return today_date, hist_date
+    print(f"Start date set to {datetime.today().strftime('%Y-%m-%d')} and end date set to {(datetime.today() - timedelta(days=num_of_days)).strftime('%Y-%m-%d')}")
+    return datetime.today().strftime('%Y-%m-%d'), (datetime.today() - timedelta(days=num_of_days)).strftime('%Y-%m-%d')
