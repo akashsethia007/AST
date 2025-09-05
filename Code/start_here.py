f1c22fbce4e6519b62c7996b3448887ba4b53e18
akki <akashsethia007@gmail.com>
2025-09-05 08:45:30 +0530

    'Updated the code'

--- Code/start_here.py
+++ Code/start_here.py
@@ -141,12 +141,10 @@ else:
     print("INFO :: No stocks in ST72 list")
 '''
 print("INFO :: Pushing the changes now")
-p='\\'.join(os.getcwd().split('\\')[:-1])
-# Run a Git command in that directory
-subprocess.run(["git", "status"], cwd=p, capture_output=True, text=True)
-subprocess.run(["git", "add", "."], cwd=p,check=True, capture_output=True, text=True)
-subprocess.run(["git", "commit", "-m", "'Updated the code'"], cwd=p,check=True, capture_output=True, text=True)
-subprocess.run(["git", "push"], cwd=p,check=True, capture_output=True, text=True)
+git_path = '\\'.join(os.getcwd().split('\\')[:-1])
+subprocess.run(["git", "add", "."], cwd=git_path,check=True, capture_output=True, text=True)
+subprocess.run(["git", "commit", "-m", "'Updated the code'"], cwd=git_path,check=True, capture_output=True, text=True)
+subprocess.run(["git", "push"], cwd=git_path,check=True, capture_output=True, text=True)
 '''
 print(f"INFO :: Completed the execution at {datetime.now()}")
 
