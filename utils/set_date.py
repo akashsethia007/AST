ba92e31e7cfc0aa95de4f41259c79ae8d40fdc78
akki <akashsethia007@gmail.com>
2025-11-07 17:48:36 +0530

    'Updated the code'

--- utils/set_date.py
+++ utils/set_date.py
@@ -3,4 +3,4 @@ from datetime import datetime, timedelta
 
 def set_dates(num_of_days):
     print(f"INFO  :: Start date set to {datetime.today().strftime('%Y-%m-%d')} and End date set to {(datetime.today() - timedelta(days=num_of_days)).strftime('%Y-%m-%d')}")
-    return str(datetime.today().strftime('%Y-%m-%d')), str((datetime.today() - timedelta(days=num_of_days)).strftime('%Y-%m-%d'))
+    return str(datetime.today()), str((datetime.today() - timedelta(days=num_of_days)).strftime('%Y-%m-%d'))
