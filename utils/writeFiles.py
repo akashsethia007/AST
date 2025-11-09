f2e0d5dc96628db8b705c98c6465480b17b1f415
akki <akashsethia007@gmail.com>
2025-11-09 11:37:38 +0530

    'Updated the code'

--- utils/writeFiles.py
+++ utils/writeFiles.py
@@ -3,7 +3,10 @@ import os
 def writeFiles(path, obj):
     os.makedirs(os.path.dirname(path), exist_ok=True)
     with open(path, 'w') as f:
-        for line in obj:
-            f.write(f"{line}\n")
+        if isinstance(obj, str):
+            f.write(f"{obj}\n")
+        else:
+            for line in obj:
+                f.write(f"{line}\n")
 #Add other file types and merge the same here...
 #Make sure to cover all file writes here
