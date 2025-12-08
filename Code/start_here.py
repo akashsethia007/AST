046050d73a885f6a58400737df9600d80b1469d1
akki <akashsethia007@gmail.com>
2025-12-08 06:36:28 +0530

    'Updated the code'

--- Code/start_here.py
+++ Code/start_here.py
@@ -47,7 +47,7 @@ def main():
 
     today_date, hist_date = set_dates(400)
     #ticker_list = update500tickers()
-    ticker_list = ['MBEL','MFSL','SAMBHV','AEROENTER','ICICIBANK','BRITANNIA','TVSHLTD','DABUR'] #Hardcoded for testing purpose
+    ticker_list = ['MBEL','MFSL','SAMBHV','AEROENTER','ICICIBANK','BRITANNIA','TVSHLTD','DABUR', 'SHRIPISTON'] #Hardcoded for testing purpose
     print(f"INFO  :: Created the list of top MCAP companies at {datetime.now()}")
     indicator_signals = []
     counter = 0
