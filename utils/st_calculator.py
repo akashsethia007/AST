51e92ad4a98219b2f88b38d4fe897b466fa52693
akki <akashsethia007@gmail.com>
2026-03-18 08:57:06 +0530

    Updated the code

--- utils/st_calculator.py
+++ utils/st_calculator.py
@@ -1,4 +1,5 @@
-import pandas_ta as ta
+import pandas_ta_classic as ta
+#pip install pandas-ta-classic
 
 def st_value(df, length, multiplier):
     st = ta.supertrend(high=df['High'], low=df['Low'], close=df['Close'], length=length, multiplier=multiplier)
